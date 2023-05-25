97877cafa7d87b57801dabcbf6c298b9460b7d6a
dib <iluaboltov@gmail.com>
2023-05-25 18:04:25 +0300

    WP

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'katex/dist/katex.min.css';
+import { InlineMath } from 'react-katex';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const nerdamer = require('nerdamer/all.min')
+const App = () => {
+    const [isOpenModal, setIsOpen] = useState(false)
+    const [textContext, setContext] = useState('')
+    const triggerModal = () =>{
+        setIsOpen(!isOpenModal)
+        return;
+    }
+    const Modal = ({textContext}: {textContext: string}) => {
+        return(
+            <div className={'modal'}>
+                <div className="modalContent">
+                    <div>Your result is:</div>
+                    <div id="mathContent">
+                        <InlineMath math={textContext}/>
+                    </div>
+                    <div onClick={triggerModal} className={'closeButton'}>X</div>
+                </div>
+            </div>
+        )
+    }
+    const calculate = () =>{
+        if(textContext !== ''){
+            setContext('')
+        }
+        let x = (document.getElementById('symb') as HTMLInputElement).value[1] || (document.getElementById('symb') as HTMLInputElement).value[0] || "x";
+        let expression = (document.getElementById('exp') as HTMLInputElement).value;
+        if (x !== null && expression !== null){
+            console.log(x)
+            let res = nerdamer.integrate(expression, x)
+            setContext(res.toString())
+            triggerModal()
+        }
+    }
+    return (
+        <div className="App">
+            <div className={'content'}>
+                <span className={'symbol'}>&#8747;</span>
+                <input id={'exp'} type="text" className={'expression'} placeholder={'x^2 + 2x + 1'}/>
+                <input id={'symb'} type="text" className={'variable'} placeholder={'dx'} maxLength={2}/>
+                <button className={'button'} onClick={calculate}>Solve</button>
+            </div>
+            {isOpenModal && <Modal textContext={textContext}/>}
+        </div>
   );
 }
 
